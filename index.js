1d227a128290e7508b0811e74f385ba462267208
BabaDelNorte <runk@free.fr>
2025-01-08 11:29:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
+response.end("Coucou Monde!");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
